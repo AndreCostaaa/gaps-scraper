db0550cb41fdd25bd74c60d80f70da7cc646d501
André Costa <toscmen.com@gmail.com>
2024-03-30 02:46:44 +0100

    parsers

--- parser/grades.py
+++ parser/grades.py
@@ -1,48 +1,36 @@
 from dataclasses import dataclass
 from bs4 import BeautifulSoup
-
-
-@dataclass
-class Grade:
-    course: str
-    grade: str
-    name: str
-
-
-def parse_headers(html) -> list:
-    soup = BeautifulSoup(html, "html.parser")
-    headers = soup.find(attrs={"class": "bigheader"})
-    headers = []
-    for header in headers:
-        headers.add(header.text)
-
-    return headers
+from app_types.grades import Grade
+from app_types.modules import Module
 
 
 def parse_grades(html) -> list:
     soup = BeautifulSoup(html, "html.parser")
     table = soup.find("table", {"class": "displayArray"})
-    lines = table.text.split("\n")
-    module_headers = set(parse_headers(html))
-    results = {}
-    averages = {}
-    module = ""
-    for i, line in enumerate(lines):
-        is_header = line in module_headers
-        is_grade = "%" in line
+    if not table:
+        return []
+
+    rows = table.find_all("tr")
+    grades = []
+    modules = []
+    curr_module = ""
+    for row in rows:
+        is_header = "bigheader" in str(row)
+        is_grade = "bodyCC" in str(row)
 
         if is_grade:
-            grade_name = lines[i - 1]
-            splitted = line.split(" ")
-            try:
-                float(splitted[-1])
-                results[module + " " + grade_name] = f"{splitted[-1]} ({splitted[0]})"
-            except ValueError:
-                pass
+            cols = row.find_all("td")
+            if len(cols) < 5:
+                continue
+            grade_name = cols[1].text
+            class_average = cols[2].text
+            grade = cols[4].text
+            if grade == "-":
+                continue
+            grades.append(Grade(curr_module, grade, grade_name, class_average))
+        elif is_header:
+            line_split = row.text.split(" ")
+            curr_module = line_split[0]
+            modules.append(Module(curr_module, line_split[-1]))
 
-        if is_header:
-            line_split = line.split(" ")
-            module = line_split[0]
-            averages[module] = line_split[-1]
-
-    return results, averages
+    return grades, modules
--- parser/student.py
+++ parser/student.py
@@ -1,7 +1,7 @@
 def parse_student_id(html: str) -> str:
-    STUDENT_ID_CONST_NAME = "/consultation/controlescontinus/consultation.php?"
-    STUDENT_ID_START_TOKEN = "="
-    STUDENT_ID_END_TOKEN = '">'
+    STUDENT_ID_CONST_NAME = "const DEFAULT_STUDENT_ID"
+    STUDENT_ID_START_TOKEN = "= "
+    STUDENT_ID_END_TOKEN = ";"
 
     identifier = html.find(STUDENT_ID_CONST_NAME)
     start = html.find(STUDENT_ID_START_TOKEN, identifier)
