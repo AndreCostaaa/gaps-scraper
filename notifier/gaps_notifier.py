de11a1a12c809b0717ef87cebf76d957f5907ef9
André Costa <toscmen.com@gmail.com>
2024-03-30 02:47:03 +0100

    email and api notifications

---
+++ notifier/gaps_notifier.py
@@ -0,0 +1,44 @@
+import os
+import requests
+
+from app_types.grades import Grade
+from notifier.api_notification import send_api_notification
+
+
+class GapsNotifierHandler:
+    def __init__(self):
+        self.url = os.environ.get("GAPS_NOTIFIER_URL", "")
+        if self.url[-1] == "/":
+            self.url = self.url[:-1]
+        self.user_id = None
+        try:
+            self.user_id = int(os.environ.get("GAPS_NOTIFIER_USER_ID", ""))
+        except ValueError:
+            pass
+
+        self.access_token = os.environ.get("GAPS_NOTIFIER_TOKEN", None)
+        if self.access_token is None:
+            self.access_token = self.fetch_gaps_notifier_access_token()
+        else:
+            self.access_token = self.access_token.replace("Bearer ", "")
+
+    def build_url(self, path: str) -> str:
+        return f"{self.url}/api/{path}"
+
+    def fetch_gaps_notifier_access_token(self) -> str:
+        req = requests.post(self.build_url("token"), json={"user_id": self.user_id})
+        if req.status_code != 200:
+            return None
+        return req.json()["access_token"]
+
+    def send_notification_to_gaps_notifier(self, grade: Grade) -> bool:
+        if not self.access_token or not self.url:
+            return False
+
+        data = grade.to_dict()
+        data["class_average"] = float(data["class_average"])
+        status_code = send_api_notification(
+            self.build_url("grade"), self.access_token, data
+        )
+
+        return status_code == 201 or status_code == 409
---
+++ notifier/notifier.py
@@ -0,0 +1,15 @@
+from notifier.email import send_email_notification
+from notifier.gaps_notifier import GapsNotifierHandler
+from app_types.grades import Grade
+
+from app_types.modules import Module
+
+gaps_notifier = GapsNotifierHandler()
+
+
+def notify(grades: list[Grade], modules: list[Module]):
+    if not grades:
+        return
+    send_email_notification(grades, modules)
+    for grade in grades:
+        gaps_notifier.send_notification_to_gaps_notifier(grade)
