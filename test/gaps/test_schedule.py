13bea4abd1959a17cb7e4a50398317eff0e6ea15
André Costa <toscmen.com@gmail.com>
2024-06-30 12:58:00 +0200

    schedule parser

--- test/gaps/test_schedule.py
+++ test/gaps/test_schedule.py
@@ -2,10 +2,9 @@ import os
 import unittest
 
 from gaps.login import login
-from gaps.schedule import fetch_classes
+from gaps.schedule import fetch_classes, fetch_current_schedule
 
 
-@unittest.skip("This is not a unit test.")
 class TestScheduleFetching(unittest.TestCase):
 
     session_id: str
@@ -20,9 +19,16 @@ class TestScheduleFetching(unittest.TestCase):
             raise Exception("Couldn't login")
         cls.session_id = session_id
 
+    @unittest.skip("This is not a unit test.")
     def test_fetch(self):
         classes = fetch_classes(self.session_id)
         print(classes)
+
+    @unittest.skip("This is not a unit test.")
+    def test_fetch_schedule(self):
+        schedule = fetch_current_schedule(self.session_id)
+        print(schedule.classes)
+
 
 
 if __name__ == "__main__":
