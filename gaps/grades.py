e38ee668df7652b08ca623ca07d65d9a92b7ef6c
André <andre_miguel_costa@hotmail.com>
2024-09-18 20:56:28 +0200

    feat: new grades fetching url

--- gaps/grades.py
+++ gaps/grades.py
@@ -2,6 +2,7 @@ from gaps.gaps import fetch_html
 from parser.grades import parse_grades
 from app_types.grades import Grade
 from app_types.modules import Module
+import json
 
 
 def __fetch_html(session_id, student_id, year) -> str:
@@ -9,14 +10,16 @@ def __fetch_html(session_id, student_id, year) -> str:
         f"consultation/controlescontinus/consultation.php?idst={student_id}&year={year}",
         session_id,
         data={
-            "rs": "smartReplacePart",
-            "rsargs": '["result","result",null,null,null,null]',
+            "rs": "replaceHtmlPart",
+            "rsargs": '["result",null]',
         },
         is_post=True,
     )
 
-    html = html.replace(r"\"", '"').replace(r"\/", "/")
-    return html
+    html = html.replace("+:", "")
+    obj = json.loads(json.loads(html))
+
+    return obj["parts"]["result"]
 
 
 def fetch_grades(session_id, student_id, year) -> tuple[list[Grade], list[Module]]:
