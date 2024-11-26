65ba20a67907b331f9bef8d93ba19e41b17c270d
André <andre_miguel_costa@hotmail.com>
2024-11-26 22:07:52 +0100

    feat(school_year): trimester utility functions

--- utils/school_year.py
+++ utils/school_year.py
@@ -10,3 +10,32 @@ def get_current_school_year() -> int:
         year -= 1
 
     return year
+
+
+def get_current_trimester() -> int:
+
+    now = datetime.now()
+    if now.month == 8 or (now.month == 9 and now.day < 20):
+        return 0
+    if now.month > 9 or now.month < 2:
+        return 1
+    return 3
+
+
+def get_next_trimester() -> int:
+    now = datetime.now()
+    if now.month == 8 or (now.month == 9 and now.day < 20):
+        return 1
+    if now.month > 9 or now.month < 2:
+        return 3
+    return 0
+
+
+def trimester_name(trimester: int) -> str:
+    if trimester == 0:
+        return "Summer"
+    if trimester == 1:
+        return "S1"
+    if trimester == 3:
+        return "S2"
+    return f"Unknown trimester ({trimester})"
